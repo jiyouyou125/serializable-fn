b0f69b77fe3ef2a56f673252bf9ff48dfdaa2cfe
Soren Macbeth <soren@dopeness.org>
2014-02-15 17:05:26 -0800

    bump to 0.0.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/serializable-fn "0.0.3-SNAPSHOT"
+(defproject yieldbot/serializable-fn "0.0.3"
   :url "http://github.com/technomancy/serializable-fn"
   :description "Serializable functions in Clojure"
   :min-lein-version "2.0.0"
@@ -9,4 +9,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.twitter/chill-java "0.3.5"]
-                 [com.twitter/carbonite "1.3.3-SNAPSHOT"]])
+                 [com.twitter/carbonite "1.3.3"]])
