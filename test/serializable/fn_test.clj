16a147744b43a0a64e7997fc9d87f44c731265e3
qaston <qaston@gmail.com>
2015-03-20 16:14:05 +0100

    Store namespace, line and col number in stacktraces for easier debugging

--- test/serializable/fn_test.clj
+++ test/serializable/fn_test.clj
@@ -15,10 +15,10 @@
   (is (= 2 (dinc 0))))
 
 (deftest metadata-fns-return-source
-  (is (.contains (:serializable.fn/source (meta dinc))(pr-str dinc-list) )))
+  (is (.contains (:serializable.fn/source (meta dinc))(str (first (drop 2 dinc-list))) )))
 
 (deftest printing-fns-show-source
-    (is (.contains (pr-str dinc) (pr-str dinc-list))))
+    (is (.contains (pr-str dinc) (str (first (drop 2 dinc-list))))))
 
 (deftest preserve-reader-metadata
   (is (number? (:serializable.fn/line (meta dinc)))))
@@ -30,6 +30,12 @@
 
 (deftest serializable-fn-roundtrip!!!111eleven
   (is (= 2 (round-trip dinc 0))))
+
+(deftest stacktraces-contain-line-col
+  (try ((fn []
+        (throw (RuntimeException.))))
+       (catch RuntimeException e
+         (is (re-matches #".*_test_sfn_\d{1,3}_\d{1,3}_.*" (str (first (.getStackTrace e))) )))))
 
 (deftest roundtrip-with-lexical-nonconst-context
   (let [x 10, y (inc x)]
