ac9f8d1bc66d2316a7cf015e958c72c4c1001289
Soren Macbeth <soren@dopeness.org>
2015-05-17 16:39:32 -0700

    update deps and bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/serializable-fn "0.0.7"
+(defproject yieldbot/serializable-fn "0.0.8"
   :url "http://github.com/sorenmacbeth/serializable-fn"
   :description "Serializable functions in Clojure"
   :min-lein-version "2.0.0"
@@ -9,9 +9,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [com.twitter/chill-java "0.3.5"]
-                 [com.twitter/carbonite "1.3.3"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [com.twitter/chill-java "0.5.0"]
+                 [com.twitter/carbonite "1.4.0"]]
   :profiles {:dev
              {:dependencies [[ch.qos.logback/logback-classic "1.1.2"]
                              [criterium "0.4.3"]]}
