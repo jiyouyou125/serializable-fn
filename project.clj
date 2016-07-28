a3b073d92a4c2bdb9ac402da08e808b40a01a6dd
Soren Macbeth <soren@dopeness.org>
2016-07-28 00:08:37 -0700

    0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/serializable-fn "0.1.1"
+(defproject yieldbot/serializable-fn "0.1.2"
   :url "http://github.com/sorenmacbeth/serializable-fn"
   :description "Serializable functions in Clojure"
   :min-lein-version "2.0.0"
@@ -10,8 +10,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [com.twitter/chill-java "0.5.0"]
-                 [com.twitter/carbonite "1.4.0"]]
+                 [com.twitter/chill-java "0.8.0"]
+                 [com.twitter/carbonite "1.5.0"]]
   :profiles {:dev
              {:dependencies [[ch.qos.logback/logback-classic "1.1.2"]
                              [criterium "0.4.3"]]}
